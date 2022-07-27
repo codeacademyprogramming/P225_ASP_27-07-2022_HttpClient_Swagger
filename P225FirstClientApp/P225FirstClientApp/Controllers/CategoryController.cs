e4e76406f996e6bde40d8f2d092e0c3dc4d9e15e
Hamid V. Mammadov <Hamid.mammadov@code.local>
2022-07-27 17:19:28 +0400

    First Commit

---
+++ P225FirstClientApp/P225FirstClientApp/Controllers/CategoryController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using P225FirstClientApp.ViewModels.CategoryVMs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P225FirstClientApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            string categoryListUrl = "http://localhost:34689/api/categories";
+
+            HttpResponseMessage httpResponseMessage = null;
+
+            using(HttpClient httpClient = new HttpClient())
+            {
+                string token = Request.Cookies["AuthToken"];
+
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                httpResponseMessage = await httpClient.GetAsync(categoryListUrl);
+            }
+
+            if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+            {
+                string content = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                List<CategoryIndexVM> categoryIndexVMs = JsonConvert.DeserializeObject<List<CategoryIndexVM>>(content);
+
+                return View(categoryIndexVMs);
+            }
+
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryCreateVM categoryCreateVM)
+        {
+            categoryCreateVM.Sekil = "Test.jpg";
+
+            string categoryPostUrl = "http://localhost:34689/api/categories";
+
+            HttpResponseMessage httpResponseMessage = null;
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string token = Request.Cookies["AuthToken"];
+
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                string content = JsonConvert.SerializeObject(categoryCreateVM);
+
+                StringContent stringContent = new StringContent(content, Encoding.UTF8, "application/json");
+
+                httpResponseMessage = await httpClient.PostAsync(categoryPostUrl, stringContent);
+            }
+
+            if (httpResponseMessage.StatusCode == HttpStatusCode.Created)
+            {
+                return RedirectToAction("index");
+            }
+
+            return RedirectToAction("index");
+        }
+    }
+}
